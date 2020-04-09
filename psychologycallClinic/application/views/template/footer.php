df0a72388143a6bf2c995b3af3242c073444d417
Ajeung Angsaweni <ajeung11@gmail.com>
2020-04-09 19:30:19 +0700

    coba ubah footer

--- psychologycallClinic/application/views/template/footer.php
+++ psychologycallClinic/application/views/template/footer.php
@@ -1,6 +1,6 @@
 <div class="fixed-bottom">
         <div class="copyright">
-        © 2020 Copyright Text : 
+        © 2020 Copyright Text : Baru 
         <a class="stretched-link right" href="http://localhost/psychologycallClinic/psychologycallClinic/"> PsychologyCallClinic</a>
         </div>
     </div>
