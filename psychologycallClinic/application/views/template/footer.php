77db3b386b011111183f6646678e5be3c46ec559
Ajeung Angsaweni <ajeung11@gmail.com>
2020-04-10 21:34:10 +0700

    edit footer

--- psychologycallClinic/application/views/template/footer.php
+++ psychologycallClinic/application/views/template/footer.php
@@ -1,6 +1,11 @@
-<div class="fixed-bottom">
+<footer id="sticky-footer" class="py-2" style="background-color:#f57b51; color: white;">
+    <div class="container text-center">
+        <small>&copy; 2020 Copyright: Psychology Call Clinic</small>
+     </div>
+</footer>
+<!-- <div class="fixed-bottom">
         <div class="copyright">
-        © 2020 Copyright Text : Baru 
+        © 2020 Copyright Text
         <a class="stretched-link right" href="http://localhost/psychologycallClinic/psychologycallClinic/"> PsychologyCallClinic</a>
         </div>
-    </div>
+    </div> -->
