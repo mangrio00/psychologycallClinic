f2db88e7452f2132cf7db990c54e0ab2da967714
Ajeung Angsaweni <ajeung11@gmail.com>
2020-04-10 14:42:53 +0700

    buat homepage

--- psychologycallClinic/application/views/default/home.php
+++ psychologycallClinic/application/views/default/home.php
@@ -1,13 +1,272 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>Home</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </head>
+
 <body>
-<br>
-<br>
-    <h1>Selamat Datang COba </h1>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">Psychology Call Clinic</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">Counselor</a>
+          </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Log In</a>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn btn-outline-secondary">Sign Up</button>
+            </li>
+          </ul>
+        </div>
+    </nav>
+
+    <!-- Carousel -->
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="images/home5.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
+        <div class="carousel-item">
+          <img src="images/home7.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
+        <div class="carousel-item">
+          <img src="images/home12.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
+        <div class="carousel-item">
+          <img src="images/home9.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+
+    <!-- Introduction -->
+    <div class="row" style="margin: 20px; color: gray; padding-top: 50px; padding-bottom: 50px;">
+      <div class="col-sm-4">
+        <img src="images/dokter1.png" class="d-block w-100" alt="...">
+      </div>
+      <div class="col-sm-8">
+        <br>
+        <h2>Welcome to Psychology Call Clinic!</h2>
+        <br>
+        <p class="lead" style="font-size: 18px;">
+          We are dedicated to providing individualized psychiatric services in a caring,<br>
+          confidential and respectful environment.<br><br> 
+          Our greatest joy comes from helping our clients realize their personal goals<br>
+          and achieve successful outcomes in their lives.
+        </p>        
+      </div>
+    </div>
+
+    <!-- Condition Treated -->
+    <div class="container-fluid" style="background-color: #C4C4C4; padding: 50px;">
+      <div class="heading" style="color: #584F4F;">
+        <h2 style="font-weight: normal;">Condition Treated</h2>
+      </div>
+      <div class="row" style="margin: 20px; color: gray;">
+        <div class="col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item">ADD/ADHD</li>
+          <li class="list-group-item">Alzhiemer’s Disease</li>
+          <li class="list-group-item">Anxiety</li>
+          <li class="list-group-item">Bipolar Disorders</li>
+          <li class="list-group-item">Depression</li>
+          <li class="list-group-item">Grief and Loss</li>
+        </ul>
+        </div>
+        <div class="col-sm-6">
+        <ul class="list-group">
+          <li class="list-group-item">Life Transitions</li>
+          <li class="list-group-item">OCD</li>
+          <li class="list-group-item">Personality Disorders</li>
+          <li class="list-group-item">Treatment Resistant Depression</li>
+          <li class="list-group-item">PTSD/Trauma</li>
+          <li class="list-group-item">Relationship Conflicts</li>
+        </ul>        
+        </div>
+      </div>
+    </div>
+
+    <!-- Cards -->
+    <div class="container-fluid" style="padding: 50px;">
+      <div class="heading" style="color: #584F4F;">
+        <h2><center>Our Doctors</center></h2>
+      </div>
+      <br>
+      <center>
+        <div class="row" style="margin: 20px; color: gray;">
+          <div class="col-sm-4">
+            <img src="images/dokter10.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">
+          </div>
+          <div class="col-sm-4">
+            <img src="images/dokter2.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">     
+          </div>
+          <div class="col-sm-4">
+            <img src="images/dokter4.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">      
+          </div>
+        </div>
+        <div class="row" style="margin: 20px; color: gray;">
+          <div class="col-sm-4">
+            <img src="images/dokter5.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">
+          </div>
+          <div class="col-sm-4">
+            <img src="images/dokter9.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">    
+          </div>
+          <div class="col-sm-4">
+            <img src="images/dokter7.jpg" class="card-img-top rounded-circle" alt="..." style="height:200px; width:200px">      
+          </div>
+        </div>
+      </center>
+      
+    </div>
+
+
+
+
+
+    <!-- <div class="container-fluid" style="background-color: #E8DACD; padding: 50px;">
+      <div class="heading" style="color: #584F4F;">
+        <h2><center>MEET THE TEAM</center></h2>
+      </div>
+      <br>
+      <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="card-group" style="margin-right: 100px; margin-left: 100px;">
+              <div class="card">
+                <img src="images/dokter1.png" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/dokter2.jpg" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/dokter4.jpg" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <div class="card-group" style="margin-right: 100px; margin-left: 100px;">
+              <div class="card">
+                <img src="images/dokter5.jpg" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/dokter9.jpg" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+              </div>
+              <div class="card">
+                <img src="images/dokter7.jpg" class="card-img-top" alt="..." height="250px" width="250px">
+                <div class="card-body">
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div> -->
+
+    <!-- Contact Us -->
+    <div class="container-fluid" style="padding: 50px; background-color: #C4C4C4; color: #584F4F">
+      <div class="heading">
+        <h2 style="font-weight: normal;">Contact Us</h2>
+      </div>
+      <div class="row">
+        <div class="col-sm-6" style="padding-top: 50px; padding-left: 30px;">
+          <h6>Phone</h6>
+          <p>021-3456 9876</p>
+          <br>
+          <h6>Email</h6>
+          <p>psychologycall@email.com</p>
+          <br>
+          <h6>Location</h6>
+          <p>Jl. Padjajaran No. 63</p>
+          <br>
+        </div>
+        <div class="col-sm-6" style="padding-left: 150px;">
+          <h5>Opening Hours</h5>
+          <br>
+          <table class="table table-hover" style="width: 400px; color: gray;">
+            <tr>
+              <td>Monday</td>
+              <td>-</td>
+              <td>08.00 - 17.00</td>
+            </tr>
+            <tr>
+              <td>Tuesday</td>
+              <td>-</td>
+              <td>08.00 - 17.00</td>
+            </tr>
+            <tr>
+              <td>Wednesday</td>
+              <td>-</td>
+              <td>08.00 - 17.00</td>
+            </tr>
+            <tr>
+              <td>Thursday</td>
+              <td>-</td>
+              <td>08.00 - 17.00</td>
+            </tr>
+            <tr>
+              <td>Friday</td>
+              <td>-</td>
+              <td>08.00 - 17.00</td>
+            </tr>  
+          </table>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer -->
+    <footer id="sticky-footer" class="py-2" style="background-color: black; color: gray;">
+      <div class="container text-center">
+        <small>&copy; 2020 Copyright: Psychology Call Clinic</small>
+      </div>
+    </footer>
 </body>
 </html>
